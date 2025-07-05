08325b191c6ba3636c648393dc417ae97ace4fb9
cacko <alex@cacko.net>
2025-07-05 05:04:03 +0100

    Give me a break, it's 2am.  But it works now.

--- views/layouts/_error.php
+++ views/layouts/_error.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
 
-    <title>{{ app.name }}</title>
+    <title>yii.cacko.net</title>
 
     <!-- Fonts -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
