ed4cf7cb93f86c9434f6333cedacacdee1f3c42b
cacko <alex@cacko.net>
2025-07-05 05:17:21 +0100

    whatthecommit.com’s server IP address could not be found.

--- modules/fullscreen/components/LoremFlickrProvider.php
+++ modules/fullscreen/components/LoremFlickrProvider.php
@@ -10,26 +10,19 @@ class LoremFlickrProvider extends Base
     /**
      * @var string
      */
-    public const BASE_URL = 'https://loremflickr.com';
+    public const BASE_URL = 'https://picsum.photos';
 
-    /**
-     * @var array
-     *     */
-    protected static $categories = [
-        'abstract', 'animals', 'business', 'cats', 'city', 'food', 'nightlife',
-        'fashion', 'people', 'nature', 'sports', 'technics', 'transport',
-    ];
 
     public static function loremFlickrUrl(
         $width = 320,
         $height = 240
-    ) {
+    ): string
+    {
         return sprintf(
-            '%s/%d/%d/%s?lock=%d',
+            '%s/%d/%d.webp?random=%d',
             self::BASE_URL,
             $width,
             $height,
-            static::$categories[array_rand(static::$categories)],
             rand(0, getrandmax())
         );
     }
