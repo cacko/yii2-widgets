e2b67caa27e81f0cc4cd1e4393b309cb4bede090
cacko <alex@cacko.net>
2025-07-05 04:58:41 +0100

    it is hump day _^_

--- views/layouts/error.php
+++ views/layouts/error.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
 
-    <title>Mutanti.com</title>
+    <title>{{ app.name }}</title>
 
     <!-- Fonts -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
